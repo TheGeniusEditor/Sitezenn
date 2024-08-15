dea22c9076bf850fcb30cb414f76a4fd9fbc37d6
Arun Kumar <arunnayaka99@gmail.com>
2024-08-15 23:45:07 +0530

    database changes

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/sitezenn", {
-   
+// Replace <username>, <password>, and <cluster-url> with your Atlas credentials and cluster information
+const atlasUri = "mongodb+srv://arunnayaka99:jCtXf2z8LYKn2soC@cluster0.wav54.mongodb.net/sz?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(atlasUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
-    console.log(`connection successful`);
+    console.log(`Connection successful`);
 }).catch((e) => {
-    console.log(`no connection`);
-})
+    console.log(`No connection: ${e.message}`);
+});
